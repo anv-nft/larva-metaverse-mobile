8e0bdd1a32aa5ce0a58719d262ccd5e9dba55e0d
YunJeongJoo <yjj4899@aniverse.io>
2022-11-30 10:08:05 +0900

    버튼수정

--- js/common.js
+++ js/common.js
@@ -4,10 +4,16 @@ function modalOpen(name){
 function lang_change(type){
     if(type === "en"){
         $('.ko_lang').hide();
+        $('.ko_lang_btn').removeClass('active');
         $('.en_lang').show();
+        $('.en_lang_btn').addClass('active');
     }else{
         $('.en_lang').hide();
+        $('.en_lang_btn').removeClass('active');
         $('.ko_lang').show();
+        $('.ko_lang_btn').addClass('active');
+
+
     }
 }
 //url복사
